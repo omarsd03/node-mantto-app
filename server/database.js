bf7e11448e21283304eafb9f0da6213759bb7b00
Omar Salgado <Omar.Salgado@saint-gobain.com>
2020-12-09 19:50:27 -0600

    Cambio de IP en BD

--- server/database.js
+++ server/database.js
@@ -2,8 +2,9 @@
 const config = {
     user: 'Abaddon_adm',
     password: 'A5764020ADCO/1994',
-    server: 'J05SV00254001AP', 
-    database: 'Tester' 
+    server: '10.201.14.18',
+    // server: 'J05SV00254001AP', 
+    database: 'Tester'
 };
 
 module.exports = config;
